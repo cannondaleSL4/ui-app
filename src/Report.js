9fafe5979f337e209580d27642bb9ac661bacdff
dmba <dmitrii.baliasnikov@netcracker.com>
2023-03-03 13:00:35 +0300

    first commit

---
+++ src/Report.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+
+function Report() {
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/report/topten', { mode: 'cors' })
+      .then(response => response.json())
+      .then(data => setCustomers(data))
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Top 10 Customers by Overall Sum</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Overall Sum</th>
+          </tr>
+        </thead>
+        <tbody>
+          {customers.map(customer => (
+            <tr key={customer.name}>
+              <td>{customer.name}</td>
+              <td>{customer.sum}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Report;
